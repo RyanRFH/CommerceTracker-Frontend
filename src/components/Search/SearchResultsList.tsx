4a211d2f7ec2411aea9604add74fd97c78c900ee
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2024-10-08 16:04:45 +0100

    adding product update functionality

--- src/components/Search/SearchResultsList.tsx
+++ src/components/Search/SearchResultsList.tsx
@@ -5,6 +5,9 @@ import { AddToBasket } from '../../services/BasketService';
 import { getUser, loginUser } from '../../services/AccountServices';
 import { DeleteProduct } from '../../services/ProductServices';
 // import { getCookie } from '../../common/Cookies/cookies';
+import SearchResultsItem from './SearchResultsListItem';
+import SearchResultsListItem from './SearchResultsListItem';
+
 
 const SearchResultsList = (props: any) => {
 
@@ -213,71 +216,7 @@ const SearchResultsList = (props: any) => {
                                     {searchResultsArray && searchResultsArray.map((product: any, index: number) => {
                                         return (
                                             <tr className='flex flex-col md:flex-row md:w-[80%] items-center border-b h-full' key={index}>
-                                                <td className="border-gray-200 bg-white text-sm leading-8 w-full md:w-1/2 h-full flex items-center justify-center">
-                                                    <div className="flex flex-col items-center w-full">
-                                                        <div className="">
-                                                            <img className="max-w-[200px] md:max-w-[200px] max-h-[200px] md:max-h-[200px]"
-                                                                src={product.imageUrl}
-                                                                alt="product"
-                                                            />
-                                                        </div>
-                                                        <div className="md:ml-3 w-full mb-[50px]">
-                                                            <div className="flex items-center flex-col text-gray-900 whitespace-no-wrap">
-                                                                <p className='text-[15px] md:text-[30px]'>
-                                                                    {product.name}
-                                                                </p>
-                                                                <p className='text-[10px] md:text-[20px] text-slate-400 md:mt-[10px]'>
-                                                                    {product.productId}
-                                                                </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="flex flex-col items-center md:w-1/2 text-center border-gray-200 bg-white mb-[10px] mx-[10px]">
-                                                    <p className="text-gray-600 whitespace-no-wrap">
-                                                        {product.description}
-                                                    </p>
-
-                                                    <p className="text-gray-900 whitespace-no-wrap mb-[10px]">
-                                                        Quantity: {product.quantity}
-                                                    </p>
-                                                    <p className="text-gray-900 whitespace-no-wrap mb-[10px]">
-                                                        £{product.price}
-                                                    </p>
-                                                    <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight z-[0] mb-[20px]">
-                                                        <span aria-hidden
-                                                            className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
-                                                        </span>
-                                                        <span className="relative">
-                                                            {(((Date.now() - Date.parse(product.createdAt)) / 1000 / 60 / 60) / 24).toFixed()} days ago
-                                                        </span>
-                                                    </span>
-                                                    {!addProductButtonState || !userDetails?.role
-                                                        ?
-                                                        <Tooltip title="Requires login" arrow>
-                                                            <div>
-                                                                <Button disabled className="pointer-events-none">Add to basket</Button>
-                                                            </div>
-                                                        </Tooltip>
-
-                                                        :
-                                                        <Button onClick={() => addItemToBasketClickHandler(product.productId, product.name)} className="">Add to basket</Button>
-                                                    }
-                                                    <div className='mt-[10px]'>
-                                                        {!addProductButtonState || userDetails?.role !== "Admin"
-                                                            ?
-                                                            <Tooltip title="Requires admin account" arrow>
-                                                                <div>
-                                                                    <Button disabled className="pointer-events-none">Delete Product</Button>
-                                                                </div>
-                                                            </Tooltip>
-                                                            :
-                                                            <Button color='danger' onClick={() => deleteProductClickHandler(product.productId, product.name)}>Delete Product</Button>
-                                                        }
-                                                    </div>
-
-
-                                                </td>
+                                                <SearchResultsListItem product={product} userDetails={userDetails} addItemToBasketClickHandler={addItemToBasketClickHandler} addProductButtonState={addProductButtonState} deleteProductClickHandler={deleteProductClickHandler} />
                                             </tr>
                                         )
                                     })}
---
+++ src/components/Search/SearchResultsListItem.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef, useState } from 'react';
+import EditIcon from '../../assets/save-file.png'
+import { Button, Tooltip } from '@mui/joy';
+
+const SearchResultsListItem = (props: any) => {
+
+    let product = props.product;
+    let userDetails = props.userDetails;
+    let addItemToBasketClickHandler = props.addItemToBasketClickHandler;
+    let addProductButtonState = props.addProductButtonState;
+    let deleteProductClickHandler = props.deleteProductClickHandler;
+
+    const quantityElement = useRef<HTMLDivElement | null>(null);
+    let [quantitySaveMessage, setQuantitySaveMessage] = useState("");
+
+    const onQuantityUpdateClick = () => {
+        setQuantitySaveMessage("");
+        if (quantityElement.current) {
+            console.log(quantityElement.current.textContent);
+        }
+        //ADD UPDATE PRODUCT SERVICE TO SERVICES AND CALL IT HERE TO UPDATE PRODUCT
+
+        setQuantitySaveMessage("Updated");
+    };
+
+    return (
+        <>
+            <td className="border-gray-200 bg-white text-sm leading-8 w-full md:w-1/2 h-full flex items-center justify-center">
+                <div className="flex flex-col items-center w-full">
+                    <div className="">
+                        <img className="max-w-[200px] md:max-w-[200px] max-h-[200px] md:max-h-[200px]"
+                            src={product.imageUrl}
+                            alt="product"
+                        />
+                    </div>
+                    <div className="md:ml-3 w-full mb-[50px]">
+                        <div className="flex items-center flex-col text-gray-900 whitespace-no-wrap">
+                            <p className='text-[15px] md:text-[30px]'>
+                                {product.name}
+                            </p>
+                            <p className='text-[10px] md:text-[20px] text-slate-400 md:mt-[10px]'>
+                                {product.productId}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td className="flex flex-col items-center md:w-1/2 text-center border-gray-200 bg-white mb-[10px] mx-[10px]">
+                <p className="text-gray-600 whitespace-no-wrap">
+                    {product.description}
+                </p>
+                <div className='flex items-center justify-center w-full my-[10px]'>
+                    <p className="text-gray-900  ">
+                        Quantity:
+                    </p>
+                    <p ref={quantityElement} suppressContentEditableWarning={true} contentEditable="true" className='bg-gray-100 border-solid border-[1px] border-black p-[5px] ml-[5px]'>
+                        {product.quantity}
+                    </p>
+                    <button onClick={onQuantityUpdateClick}>
+                        <img src={EditIcon} className='w-[24px] ml-[5px]' />
+                    </button>
+                    <p className='text-green-500'>{quantitySaveMessage}</p>
+
+                </div>
+
+                <p className="text-gray-900 whitespace-no-wrap mb-[10px]">
+                    £{product.price}
+                </p>
+                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight z-[0] mb-[20px]">
+                    <span aria-hidden
+                        className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
+                    </span>
+                    <span className="relative">
+                        {(((Date.now() - Date.parse(product.createdAt)) / 1000 / 60 / 60) / 24).toFixed()} days ago
+                    </span>
+                </span>
+                {!addProductButtonState || !userDetails?.role
+                    ?
+                    <Tooltip title="Requires login" arrow>
+                        <div>
+                            <Button disabled className="pointer-events-none">Add to basket</Button>
+                        </div>
+                    </Tooltip>
+                    :
+                    <Button onClick={() => addItemToBasketClickHandler(product.productId, product.name)} className="">Add to basket</Button>
+                }
+                <div className='mt-[10px]'>
+                    {!addProductButtonState || userDetails?.role !== "Admin"
+                        ?
+                        <Tooltip title="Requires admin account" arrow>
+                            <div>
+                                <Button disabled className="pointer-events-none">Delete Product</Button>
+                            </div>
+                        </Tooltip>
+                        :
+                        <Button color='danger' onClick={() => deleteProductClickHandler(product.productId, product.name)}>Delete Product</Button>
+                    }
+                </div>
+
+
+            </td>
+        </>
+
+    );
+};
+
+export default SearchResultsListItem;
